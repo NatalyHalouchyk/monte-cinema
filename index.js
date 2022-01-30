ef43790eb17bb572a3baccdf6a3ccd7626154050
NatalyHalouchyk <natallia.halouchyk@gmail.com>
2022-01-30 18:59:35 +0100

    add hover and active states for buttons and input

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const btn = document.querySelectorAll("button");
 
+for (let i=0; i<btn.length; i++){
+  btn[i].addEventListener("click",()=>{
+    btn[i].classList.add("pressed");
+
+    setTimeout(()=>{
+      btn[i].classList.remove("pressed");
+    }, 200)
+  })
+}
